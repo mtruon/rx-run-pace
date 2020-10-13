9934fdbeaaa5adb58f6763aedf1cbc78f45ccafd
Michael Truong <mtruon02@mail.uoguelph.ca>
2020-10-13 13:02:49 -0400

    Added initial system component design and field component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Field from './Field'
+
+const kphToMinsKM = (kmh) => {
+  let minsKM = (60/kmh)
+  return numToMinutes(minsKM)
+}
+
+// Converts a numerical representation of some minutes
+// to the literal string representation mm:ss
+const numToMinutes = (n) => {
+  let minutes = Math.floor(n)
+  let seconds = ((n - minutes) * 60).toFixed(0)
+  return minutes + ":"  + seconds
+}
 
 const App = () => {
+  const [kph, setKPH] = useState(0)
+  const [mph, setMPH] = useState(0)
+  const [mpk, setMPK] = useState(0)
+  const [minsMI, setMinsMI] = useState(0)
+  const [fiveKM, setFiveKM] = useState(0)
+  const [tenKM, setTenKM] = useState(0)
+  const [halfMarathon, setHalfMarathon] = useState(0)
+  const [marathon, setMarathon] = useState(0)
+  const [data, setData] = useState({
+    'kph':0,
+    'mph':0,
+    'mpk':0,
+    'mpmi':0,
+    'fiveKM':0,
+    'tenKM':0,
+    'halfMarathon':0,
+    'marathon':0
+  })
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+  useEffect(() => {
+    setMPK(kphToMinsKM(kph))
+  }, [kph])
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    console.log(e.target.value, "=>",kphToMinsKM(e.target.value))
+    setKPH(e.target.value)
+  }
+
   return(
-    <div>Hello world</div>
+    <div>
+      <Field
+        name={"KM/Hour"}
+        key={"kph"}
+        value={kph}
+        handleChange={handleChange}
+      />
+      <Field
+        name={"mins/KM"}
+        key={"mpk"}
+        value={mpk}
+        handleChange={handleChange}
+      />
+    </div>
   )
 }
 
---
+++ app/javascript/components/Field.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FieldWrapper = styled.div`
+  width: 100%;
+  
+  h1 {
+    font-size: 16px;
+    font-weight: 300;
+  }
+
+  input {
+    min-height: 30px;
+    border-radius: 4px;
+    border: 1px solid #e6e6e6;
+    margin: 0 0 12px 0;
+    padding: 12px;
+    width: 96%;
+  }
+`
+
+const Field = (props) => {
+  return(
+    <FieldWrapper>
+      <h1>{props.name}</h1>
+      <input
+        onChange={props.handleChange}
+        value={props.value}
+        name={props.name}
+        min="0"
+        placeholder="How many are available?"
+      />
+    </FieldWrapper>
+  )
+}
+
+
+
+export default Field
